5678c69f308b72559300f68dce00af517d03c898
Shambhuraje Sunil Jagadale <shambhurajejagadale@gmail.com>
2026-07-30 10:58:56 +0530

    Added new local LLM's like tinyllma's also added and practiced embedding's

---
+++ 3_Embeddings/1_embedding_openai_query.py
@@ -0,0 +1,10 @@
+from langchain_openai import OpenAIEmbeddings
+from dotenv import load_dotenv
+
+load_dotenv()
+
+embedding = OpenAiEmbedding(model_name = ' text-embedding-3-large',dimension=32)
+
+res = embedding.embed_query("whats the capital of maharashtra??")
+
+print(str(res))
