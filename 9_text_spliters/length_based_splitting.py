dd431464e203396afe26024cb4d200e5d973eca1
Shambhuraje Sunil Jagadale <shambhurajejagadale@gmail.com>
2026-08-02 19:10:54 +0530

    add semantic , python , markdown code spliting methods

--- 9_text_spliters/length_based_splitting.py
+++ 9_text_spliters/length_based_splitting.py
@@ -1,4 +1,3 @@
-from langchain_community.document_loaders.base_o365 import CHUNK_SIZE
 from langchain_text_splitters import CharacterTextSplitter
 from langchain_community.document_loaders import PyPDFLoader
 
@@ -8,7 +7,7 @@ pdf = loader.load()
 splitter = CharacterTextSplitter(
     chunk_size = 100,
     chunk_overlap =0,
-    separator=''
+    separator='\n\n'
 )
 pdf_spliter = splitter.split_documents(pdf)
 
---
+++ 9_text_spliters/markdown_spliting.py
@@ -0,0 +1,42 @@
+
+from langchain_text_splitters import RecursiveCharacterTextSplitter,Language
+
+text = """
+# Project Name: Smart Student Tracker
+
+A simple Python-based project to manage and track student data, including their grades, age, and academic status.
+
+
+## Features
+
+- Add new students with relevant info
+- View student details
+- Check if a student is passing
+- Easily extendable class-based design
+
+
+## 🛠 Tech Stack
+
+- Python 3.10+
+- No external dependencies
+
+
+## Getting Started
+
+1. Clone the repo  
+   ```bash
+   git clone https://github.com/your-username/student-tracker.git
+
+"""
+spilter = RecursiveCharacterTextSplitter.from_language(
+    language=Language.MARKDOWN,
+    chunk_size =200,
+    chunk_overlap= 0
+    
+)
+
+chunk = spilter.split_text(text)
+print(len(chunk))
+print(chunk)
+
+
